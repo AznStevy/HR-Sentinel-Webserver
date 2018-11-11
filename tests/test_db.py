0870b34d168d88b63f0fdade697a735213119503
Stephen <szx2@duke.com>
2018-11-11 16:49:43 -0500

    Adds some tests + proposed structure of project.

---
+++ tests/test_db.py
@@ -0,0 +1,16 @@
+import pytest
+import hrs_db
+
+
+# define new database to not screw up production db
+
+def test_add_patient():
+    assert True
+
+
+def test_remove_patient():
+    assert True
+
+
+def test_get_patient():
+    assert True
--- tests/test_sentinel.py
+++ tests/test_sentinel.py
@@ -1,4 +1,25 @@
 import pytest
 
-def test_sentinel():
+
+def test_get_status():
+    assert True
+
+
+def test_get_heart_rate():
+    assert True
+
+
+def test_get_average():
+    assert True
+
+
+def test_get_internal_average():
+    assert True
+
+
+def test_post_new_patient():
+    assert True
+
+
+def test_post_heart_rate():
     assert True
